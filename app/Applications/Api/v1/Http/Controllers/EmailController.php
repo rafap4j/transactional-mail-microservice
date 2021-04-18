d5f3bb0608c79cb4c1a463a74fc37c09b3fc93d2
Rafael Alberti Cantoni Augusto <>
2021-04-18 16:33:26 -0300

    feat: implement endpoint for sending e-mails
    This commit implements the endpoint for sending e-mail messages
    and opens up an API entry in the route file.

--- app/Applications/Api/v1/Http/Controllers/EmailController.php
+++ app/Applications/Api/v1/Http/Controllers/EmailController.php
@@ -4,6 +4,7 @@ namespace App\Applications\Api\v1\Http\Controllers;
 
 use App\Applications\Api\v1\Http\Requests\EmailSendRequest;
 use App\Http\Controllers\Controller;
+use App\Jobs\SendEmail;
 
 /**
  * E-mail controller class.
@@ -20,5 +21,11 @@ class EmailController extends Controller
      */
     public function send(EmailSendRequest $request)
     {
+        SendEmail::dispatch($request->validated());
+
+        return response([
+            'message' => 'The e-mail message has been enqueued and will be sent soon.',
+            'service' => config('mail.default_email_service'),
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Applications\Api\v1\Http\Controllers\EmailController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    /**
+     * E-mail routes.
+     */
+    Route::prefix('email')->group(function () {
+        Route::post('send', [EmailController::class, 'send'])->name('email.send');
+    });
 });
