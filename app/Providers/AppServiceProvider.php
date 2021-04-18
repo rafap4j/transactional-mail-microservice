a0bb1d82897f106d89ce2eb727b7253bbc4144dc
Rafael Alberti Cantoni Augusto <>
2021-04-18 15:35:10 -0300

    feat: create Mailjet sending feature
    This commit creates the Mailjet handle along with container DI.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\MailjetEmailHandler;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind('Mailjet', function () {
+            return new MailjetEmailHandler(env('MAILJET_API_KEY'), env('MAILJET_SECRET_KEY'));
+        });
     }
 }
---
+++ app/Services/MailjetEmailHandler.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Interfaces\EmailHandler;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class MailjetEmailHandler implements EmailHandler
+{
+    /**
+     * Mailjet API key.
+     *
+     * @var string
+     */
+    private $mailjetApiKey;
+
+    /**
+     * Mailjet secret key.
+     *
+     * @var string
+     */
+    private $mailjetSecretKey;
+
+    /**
+     * Class constructor.
+     *
+     * @param  string  $mailjetApiKey
+     * @param  string  $mailjetSecretKey
+     */
+    public function __construct(string $mailjetApiKey, string $mailjetSecretKey)
+    {
+        $this->mailjetApiKey = $mailjetApiKey;
+        $this->mailjetSecretKey = $mailjetSecretKey;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function send(array $data)
+    {
+        $mailjetClient = new Client(
+            $this->mailjetApiKey,
+            $this->mailjetSecretKey,
+            true,
+            ['version' => 'v3.1']
+        );
+
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => config('mail.from.address'),
+                        'Name' => config('mail.from.name'),
+                    ],
+                    'To' => [
+                        ['Email' => $data['to']]
+                    ],
+                    'Subject' => $data['subject'],
+                    'TextPart' => $data['body'],
+                ]
+            ]
+        ];
+
+        try {
+            $response = $mailjetClient->post(Resources::$Email, compact('body'));
+
+            if ($response->success()) {
+                $this->log($data);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function log(array $data)
+    {
+        Log::info('E-mail sent to ' . $data['to'] . ' using ' . get_called_class() . ' on ' . Carbon::now());
+    }
+}
