51e27544bd6a6c0674189be5af00db79c87407ea
Rafael Alberti Cantoni Augusto <>
2021-04-18 16:34:39 -0300

    feat: create tests for default/fallback services

--- tests/Feature/SendEmailTest.php
+++ tests/Feature/SendEmailTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Jobs\SendEmail;
+use Exception;
 use Tests\TestCase;
 
 class SendEmailTest extends TestCase
@@ -21,5 +23,46 @@ class SendEmailTest extends TestCase
         ])->assertStatus(200);
 
         $this->assertDatabaseHas('sent_emails', $data);
+    }
+
+    /**
+     * Send an e-mail message using the default service.
+     *
+     * @return void
+     */
+    public function send_an_email_using_default_email_service()
+    {
+        $data = \App\Models\SentEmail::factory()->definition();
+
+        $response = $this->json('POST', route('email.send'), $data, [
+            'Content-Type' => 'application/json',
+        ]);
+
+        $this->assertEquals(
+            $response['data']['message']['service'],
+            config('mail.default_email_service')
+        );
+    }
+
+    /**
+     * Send an e-mail message using a fallback service.
+     *
+     * @return void
+     */
+    public function send_an_email_using_fallback_email_service()
+    {
+        $defaultEmailService = config('mail.default_email_service');
+
+        $data = \App\Models\SentEmail::factory()->definition();
+        (new SendEmail($data))->failed(new Exception());
+
+        $response = $this->json('POST', route('email.send'), $data, [
+            'Content-Type' => 'application/json'
+        ]);
+
+        $this->assertEquals(
+            $response['data']['message']['service'],
+            $defaultEmailService
+        );
     }
 }
